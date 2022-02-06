cda9b825b0d020f63f918bda4d801f04b45fbbaf
John Carter <cadsoft.dev@gmail.com>
2022-02-06 16:24:33 +0900

    update

---
+++ tests/myfirstsolanaapp.js
@@ -0,0 +1,52 @@
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+describe('myfirstsolanaapp', () => {
+  /* create and set a Provider */
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Myfirstsolanaapp;
+  let _baseAccount;
+  
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const program = anchor.workspace.Myfirstsolanaapp;
+    const tx = await program.rpc.initialize();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Creates a counter)", async () => {
+    /* Call the create function via RPC */
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    /* Fetch the account and check the value of count */
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 0: ', account.count.toString())
+    assert.ok(account.count.toString() == 0);
+    _baseAccount = baseAccount;
+
+  });  
+
+  it("Increments the counter", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 1: ', account.count.toString())
+    assert.ok(account.count.toString() == 1);
+  });  
+});
